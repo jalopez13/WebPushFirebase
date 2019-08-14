295233860dd69031f5079721bf38d6bb0e6a1033
Javier Lopez <javier.lopez@tmz.com>
2019-08-14 14:19:03 -0700

    Added send  notification form

--- js/main.js
+++ js/main.js
@@ -1,31 +1,120 @@
-document.addEventListener('DOMContentLoaded', event => {
-  const signinBtn = document.getElementById('signin');
-  const signoutBtn = document.getElementById('signout');
-  const FIREBASE_AUTH = firebase.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
+/**
+ * AUTHENTICATION
+ */
 
-  // sign in
-  signinBtn.addEventListener('click', function(event) {
-    FIREBASE_AUTH.signInWithPopup(provider).then(function(res) {
-      console.log(res);
-    })
-    .catch(err => console.log(err));
-  });
+const signIn = document.getElementById('signin');
+const signOut = document.getElementById('signout');
 
-  FIREBASE_AUTH.onAuthStateChanged(function(user) {
-    if (user) {
-      console.log(user);
-      signinBtn.setAttribute('hidden', true);
-      signoutBtn.removeAttribute('hidden');
+const FIREBASE_AUTH = firebase.auth();
+const FIREBASE_MESSAGING = firebase.messaging();
+const FIREBASE_DATABASE = firebase.database();
+
+const handleSignIn = () => {
+FIREBASE_AUTH.signInWithPopup( new firebase.auth.GoogleAuthProvider() );
+};
+
+const handleSignOut = () => {
+  FIREBASE_AUTH.signOut();
+  };
+  
+const handleAuthStateChange = user => {
+  if (user) {
+    signIn.setAttribute('hidden', true);
+    signOut.removeAttribute('hidden');
+    checkSubscription();
+  } else {
+    signIn.removeAttribute('hidden');
+    signOut.setAttribute('hidden', true);
+  }
+};
+
+const handleTokenRefresh = () => {
+  return FIREBASE_MESSAGING.getToken()
+  .then(() => FIREBASE_MESSAGING.getToken())
+  .then(token => {
+    FIREBASE_DATABASE.ref('/tokens').push({
+      token,
+      uid: FIREBASE_AUTH.currentUser.uid
+    });
+  })
+  .catch(error => console.log("User didn't give permission :(", error));
+}
+
+FIREBASE_AUTH.onAuthStateChanged(handleAuthStateChange);
+FIREBASE_MESSAGING.onTokenRefresh(handleTokenRefresh)
+signIn.addEventListener('click', handleSignIn);
+signOut.addEventListener('click', handleSignOut);
+
+/**
+ * SUBSCRIBE TO NOTIFICATIONS
+ */
+
+const subscribeBtn = document.getElementById('subscribe');
+
+const handleSubscribeToNotifications = () => {
+  // ask permission
+  FIREBASE_MESSAGING.requestPermission()
+  // get subscription token
+  .then(() => FIREBASE_MESSAGING.getToken())
+  .then(token => handleTokenRefresh())
+  .then(() => checkSubscription())
+  .catch(error => console.log("User didn't give permission :(", error))
+  
+
+
+};
+
+subscribeBtn.addEventListener('click', handleSubscribeToNotifications);
+
+/**
+ * UNSUBSCRIBE TO NOTIFICATIONS
+ */
+
+const unsubscribeBtn = document.getElementById('unsubscribe');
+
+const handleUnsubscribeToNotifications = () => {
+  FIREBASE_MESSAGING.getToken()
+  .then(token => FIREBASE_MESSAGING.deleteToken(token))
+  .then(() => FIREBASE_DATABASE.ref('/tokens').orderByChild('uid').equalTo(FIREBASE_AUTH.currentUser.uid).once('value'))
+  .then(snapshot => {
+    console.log('Snapshot: ', snapshot.val());
+    const key = Object.keys( snapshot.val() )[0];
+    return FIREBASE_DATABASE.ref('/tokens').child(key).remove();
+  })
+  .then(() => checkSubscription())
+  .catch(error => console.log("Unsubscribed failed :(", error));
+}
+
+const checkSubscription = () => {
+  FIREBASE_DATABASE.ref('/tokens').orderByChild('uid').equalTo(FIREBASE_AUTH.currentUser.uid).once('value')
+  .then(snapshot => {
+    if (snapshot.val()) {
+      subscribeBtn.setAttribute('hidden', true);
+      unsubscribeBtn.removeAttribute('hidden');
     } else {
-      console.log('Logged Out Mate!');
-      signinBtn.removeAttribute('hidden');
-      signoutBtn.setAttribute('hidden', true);
+      subscribeBtn.removeAttribute('hidden');
+      unsubscribeBtn.setAttribute('hidden', true);
     }
-  });
+  })
+};
 
-  // sign out
-  signoutBtn.addEventListener('click', function() {
-    FIREBASE_AUTH.signOut();
+unsubscribeBtn.addEventListener('click', handleUnsubscribeToNotifications);
+
+
+/**
+ * SEND NOTIFICATIONS
+ */
+
+const sendBtn = document.getElementById('send-btn');
+const notificationMessage = document.getElementById('notification-form').value;
+
+const handleSendNotification = (event) => {
+  event.preventDefault();
+
+  FIREBASE_DATABASE.ref('/notifications').push({
+    user: FIREBASE_AUTH.currentUser.displayName,
+    message: notificationMessage
   });
-});
+};
+
+sendBtn.addEventListener('submit', handleSendNotification);
