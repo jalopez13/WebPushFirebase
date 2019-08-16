20798cd6a1a6c22e63c0633968495309b365fe48
Javier Lopez <javier.lopez@tmz.com>
2019-08-16 15:38:59 -0700

    Added receive message feature.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,73 @@
+/* eslint-disable consistent-return */
+/* eslint-disable promise/no-nesting */
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+exports.sendNotifications = functions.database.ref('/notifications/{notificationId}').onWrite(async (change) => {
+  if (!change.after) return;
+
+  const NOTIFICATION_SNAPSHOT = change.after;
+  const projectId = admin.instanceId().app.options.projectId;
+
+  const payload = {
+    notification: {
+      title: `New message from  ${NOTIFICATION_SNAPSHOT.val().user}`,
+      body: NOTIFICATION_SNAPSHOT.val().message,
+      icon: NOTIFICATION_SNAPSHOT.val().userImage,
+      click_action: `https://${projectId}.firebaseapp.com`
+    }
+  };
+
+  var options = {
+    priority: 'high',
+    timeToLive: 60 * 60 * 24
+  };
+
+  const clearInvalidTokens = (results, tokensWithKey) => {
+    const invalidTokens = [];
+    results.forEach((result, index) => {
+      if (!result.error) return;
+
+      console.error('Error with token: ', tokensWithKey[i].token);
+
+      switch(result.error.code) {
+        case 'messaging/invalid-registration-token':
+          break;
+        case 'messaging/registration-token-not-registered':
+          invalidTokens.push(admin.database().ref('/tokens').child(tokensWithKey[i].key).remove());
+          break;
+        default:
+          break;
+      }
+    });
+
+    return Promise.all(invalidTokens);
+  };
+
+  // eslint-disable-next-line consistent-return
+  return admin.database().ref('/tokens').once('value').then(data => {
+      if (!data.val()) return;
+
+      const snapshot = data.val();
+      const tokens = [];
+      const tokensWithKey = [];
+
+      for (let key in snapshot) {
+        tokens.push(snapshot[key].token);
+        tokensWithKey.push({
+          token: snapshot[key].token,
+          key
+        });
+      }
+
+      return admin.messaging().sendToDevice(tokens, payload, options)
+      .then(res => clearInvalidTokens(res.results, tokensWithKey))
+      .then(() => {
+        return admin.database().ref('/notifications').child(NOTIFICATION_SNAPSHOT.key).remove()
+      })
+      .then(res => console.log('Successfully sent message:', res))
+      .catch(error => console.log('Error sending message:', error));
+  });
+});
--- js/main.js
+++ js/main.js
@@ -1,30 +1,51 @@
-/**
- * AUTHENTICATION
- */
-
-const signIn = document.getElementById('signin');
-const signOut = document.getElementById('signout');
+// =======================================================
+// VARIABLES
+// =======================================================
 
 const FIREBASE_AUTH = firebase.auth();
 const FIREBASE_MESSAGING = firebase.messaging();
 const FIREBASE_DATABASE = firebase.database();
 
+const signInBtn = document.getElementById('signin');
+const signOutBtn = document.getElementById('signout');
+const subscribeBtn = document.getElementById('subscribe');
+const unsubscribeBtn = document.getElementById('unsubscribe');
+const notificationForm = document.getElementById('notification-form');
+const notificationMessage = document.getElementById('notification-message');
+const infoDiv = document.getElementById('info');
+
+// =======================================================
+// FUNCTIONS
+// =======================================================
+
+// AUTHENTICATION
+
 const handleSignIn = () => {
-FIREBASE_AUTH.signInWithPopup( new firebase.auth.GoogleAuthProvider() );
+  FIREBASE_AUTH.signInWithPopup( new firebase.auth.GoogleAuthProvider() );
 };
 
 const handleSignOut = () => {
-  FIREBASE_AUTH.signOut();
-  };
+  if (confirm('Are you sure you want to sign out?')) {
+    subscribeBtn.setAttribute('hidden', true);
+    FIREBASE_AUTH.signOut();
+  }
+};
   
 const handleAuthStateChange = user => {
   if (user) {
-    signIn.setAttribute('hidden', true);
-    signOut.removeAttribute('hidden');
+    // User is signed in
+    console.log("[ USER SIGNED IN ]");
+    signInBtn.setAttribute("hidden", "true");
+    signOutBtn.removeAttribute("hidden");
+    notificationForm.removeAttribute("hidden");
+
     checkSubscription();
   } else {
-    signIn.removeAttribute('hidden');
-    signOut.setAttribute('hidden', true);
+    // User is not signed in
+    console.log("[ USER NOT SIGNED IN ]");
+    signOutBtn.setAttribute("hidden", "true");
+    signInBtn.removeAttribute("hidden");
+    notificationForm.setAttribute("hidden", "true");
   }
 };
 
@@ -37,19 +58,20 @@ const handleTokenRefresh = () => {
       uid: FIREBASE_AUTH.currentUser.uid
     });
   })
-  .catch(error => console.log("User didn't give permission :(", error));
+  .catch(error => {
+    infoDiv.classList.add('active');
+    infoDiv.innerText = `User didn't give permission :(", ${error}`;
+
+    setTimeout(function() {
+      infoDiv.classList.remove('active');
+    }, 1000);
+  });
 }
 
 FIREBASE_AUTH.onAuthStateChanged(handleAuthStateChange);
 FIREBASE_MESSAGING.onTokenRefresh(handleTokenRefresh)
-signIn.addEventListener('click', handleSignIn);
-signOut.addEventListener('click', handleSignOut);
 
-/**
- * SUBSCRIBE TO NOTIFICATIONS
- */
-
-const subscribeBtn = document.getElementById('subscribe');
+// SUBSCRIBE TO NOTIFICATIONS
 
 const handleSubscribeToNotifications = () => {
   // ask permission
@@ -58,26 +80,23 @@ const handleSubscribeToNotifications = () => {
   .then(() => FIREBASE_MESSAGING.getToken())
   .then(token => handleTokenRefresh())
   .then(() => checkSubscription())
-  .catch(error => console.log("User didn't give permission :(", error))
-  
+  .catch(error => {
+    infoDiv.classList.add('active');
+    infoDiv.innerText = `User didn't give permission :(", ${error}`;
 
-
+    setTimeout(function() {
+      infoDiv.classList.remove('active');
+    }, 5000);
+  });
 };
 
-subscribeBtn.addEventListener('click', handleSubscribeToNotifications);
-
-/**
- * UNSUBSCRIBE TO NOTIFICATIONS
- */
-
-const unsubscribeBtn = document.getElementById('unsubscribe');
+// UNSUBSCRIBE TO NOTIFICATIONS
 
 const handleUnsubscribeToNotifications = () => {
   FIREBASE_MESSAGING.getToken()
   .then(token => FIREBASE_MESSAGING.deleteToken(token))
   .then(() => FIREBASE_DATABASE.ref('/tokens').orderByChild('uid').equalTo(FIREBASE_AUTH.currentUser.uid).once('value'))
   .then(snapshot => {
-    console.log('Snapshot: ', snapshot.val());
     const key = Object.keys( snapshot.val() )[0];
     return FIREBASE_DATABASE.ref('/tokens').child(key).remove();
   })
@@ -91,30 +110,36 @@ const checkSubscription = () => {
     if (snapshot.val()) {
       subscribeBtn.setAttribute('hidden', true);
       unsubscribeBtn.removeAttribute('hidden');
+      console.log('[ USER SUBSCRIBED ]');
     } else {
       subscribeBtn.removeAttribute('hidden');
       unsubscribeBtn.setAttribute('hidden', true);
+      console.log('[ USER UNSUBSCRIBED ]');
     }
   })
 };
 
-unsubscribeBtn.addEventListener('click', handleUnsubscribeToNotifications);
-
-
-/**
- * SEND NOTIFICATIONS
- */
-
-const sendBtn = document.getElementById('send-btn');
-const notificationMessage = document.getElementById('notification-form').value;
+// SEND NOTIFICATIONS
 
 const handleSendNotification = (event) => {
   event.preventDefault();
-
   FIREBASE_DATABASE.ref('/notifications').push({
     user: FIREBASE_AUTH.currentUser.displayName,
-    message: notificationMessage
+    message: notificationMessage.value,
+    userImage: FIREBASE_AUTH.currentUser.photoURL
+  })
+  .then(() => {
+    notificationMessage.value = '';
   });
 };
 
-sendBtn.addEventListener('submit', handleSendNotification);
+
+// =======================================================
+// EVENT LISTENERS
+// =======================================================
+
+signInBtn.addEventListener('click', handleSignIn);
+signOutBtn.addEventListener('click', handleSignOut);
+subscribeBtn.addEventListener('click', handleSubscribeToNotifications);
+unsubscribeBtn.addEventListener('click', handleUnsubscribeToNotifications);
+notificationForm.addEventListener('submit', handleSendNotification);
